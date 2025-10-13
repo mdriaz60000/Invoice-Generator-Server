a06d261f6dbddf5ac4ecd0e6c5d9bcb597171e27
mdriaz60000 <mohammadriaz60000@gmail.com>
2025-10-13 17:41:57 +0600

    create project

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from "express";
+
+import config from "./app/config";
+
+const app: Application = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`Server Running on port ${config.port}`);
+});
+
+export default app;
